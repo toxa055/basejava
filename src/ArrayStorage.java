c3d787b51252a7088ab6bbeb3b71f646357877e1
toxa055 <toxa055@gmail.com>
2020-06-24 18:25:54 +0300

    implement ArrayStorage methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,59 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : getAll()) {
+            if (resume.uuid.equals(uuid))
+                return resume;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = size();
+        boolean isDeletedElementExist = false;
+        int indexOfDeletedElement = 0;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                indexOfDeletedElement = i;
+                isDeletedElementExist = true;
+                break;
+            }
+        }
+        if (isDeletedElementExist) {
+            for (int i = indexOfDeletedElement; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[size - 1] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null)
+                return i;
+        }
         return 0;
     }
 }
