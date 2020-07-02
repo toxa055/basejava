60a1317f4f98718733aa46a531056e33bd671745
toxa055 <toxa055@gmail.com>
2020-07-02 18:34:27 +0300

    add AbstractArrayStorage & SortedArrayStorage

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,25 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public abstract class AbstractArrayStorage implements Storage{
+    protected static final int STORAGE_LIMIT = 10_000;
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index != -1) {
+            return storage[index];
+        }
+        System.out.println("Resume with " + uuid + " uuid does not exist.");
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    protected abstract int getIndex(String uuid);
+
+    }
